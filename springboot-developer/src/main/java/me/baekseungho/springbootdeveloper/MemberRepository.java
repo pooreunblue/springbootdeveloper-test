5a90343c9607a00a0fe3796b240c8642c1faf566
baekseungho <mjbsh02@mju.ac.kr>
2025-03-06 16:35:32 +0900

    Feat: MemberRepository 생성

---
+++ springboot-developer/src/main/java/me/baekseungho/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,6 @@
+package me.baekseungho.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
